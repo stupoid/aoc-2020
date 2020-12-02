88112ec2baad3723f504a300e6144a2e790c7009
stupoid <kelvin.koh@hennge.com>
2020-12-02 10:26:33 +0900

    day1 pt2

--- day/1/main.py
+++ day/1/main.py
@@ -25,6 +25,13 @@ In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying the
 Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
 
 To begin, get your puzzle input. https://adventofcode.com/2020/day/1/input
+
+--- Part Two ---
+The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+
+Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
 """
 
 import functools
@@ -33,20 +40,35 @@ from operator import mul
 from typing import Iterable, Optional, Tuple
 
 
-def find_entries(input: Iterable[int]) -> Optional[Tuple[int]]:
-    combinations = itertools.combinations(input, 2)
+def find_entries_that_sum_to(
+    input: Iterable[int],
+    num_entries: int,
+    sum_to: int,
+) -> Optional[Tuple[int]]:
+    combinations = itertools.combinations(input, num_entries)
     for entries in combinations:
-        if sum(entries) == 2020:
+        if sum(entries) == sum_to:
             return entries
 
 
 def main():
-    input_gen = (int(l) for l in open("day/1/input.txt", "r"))
-    if entries := find_entries(input_gen):
+    input_array = [int(l) for l in open("day/1/input.txt", "r")]
+    n = 2
+    sum_to = 2020
+
+    if entries := find_entries_that_sum_to(input_array, n, sum_to):
         product_of_entries = functools.reduce(mul, entries)
-        print(product_of_entries)
+        print(f"product of {n} entries that sum to {sum_to}: {product_of_entries}")
     else:
-        print("not found")
+        print(f"product of {n} entries not found")
+
+    n = 3
+
+    if entries := find_entries_that_sum_to(input_array, n, sum_to):
+        product_of_entries = functools.reduce(mul, entries)
+        print(f"product of {n} entries that sum to {sum_to}: {product_of_entries}")
+    else:
+        print(f"product of {n} entries not found")
 
 
 if __name__ == "__main__":
